e32989f065120ba44051a5cbfa53e832598a13f6
Duc Pham <minhducpham.it@gmail.com>
2017-03-24 07:25:55 +0700

    add

---
+++ js/common.js
@@ -0,0 +1,45 @@
+(function($){
+  "use strict";
+
+  // Script initialization
+  $(window).load(function () {
+    /* Page loader */;
+    $('#loading').delay(600).fadeOut(1000);
+  });
+
+  // Slide Carousel
+  $(document).ready(function() {
+    $(".owl-carousel").each(function(index, el) {
+      var config = $(this).data();
+      config.navText = ['<i class="icofont icofont-thin-left"></i>','<i class="icofont icofont-thin-right"></i>'];
+      config.smartSpeed="800";
+     
+      if($(this).hasClass('owl-style2')){
+        config.animateOut="fadeOut";
+        config.animateIn="fadeIn";    
+      }
+
+      if($(this).hasClass('dotsData')){
+        config.dotsData="true";
+      }
+      
+      $(this).owlCarousel(config);
+    });
+
+  });
+
+  // ===== Scroll to Top ==== 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 200) {
+      $('#return-to-top').addClass('td-scroll-up-visible');
+    } else {
+      $('#return-to-top').removeClass('td-scroll-up-visible');
+    }
+  });
+  $('#return-to-top').click(function() {
+    $('body,html').animate({
+      scrollTop : 0
+    }, 'slow');
+  });
+
+})(jQuery); // End of use strict
